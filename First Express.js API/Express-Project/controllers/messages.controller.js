10e39adfeaaf4d32fd3551e84833aba4ee93ca1f
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2023-09-06 14:36:41 +0700

    finish section First Express.js API

---
+++ First Express.js API/Express-Project/controllers/messages.controller.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+function getMessages(req, res) {
+    res.render("messages", {
+        title: "Messages to my Friends!",
+        friend: "Elon Musk",
+    });
+}
+
+function postMessage(req, res) {
+    console.log("Updating messages...");
+}
+
+module.exports = {
+    getMessages,
+    postMessage,
+};
